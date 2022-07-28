a67f3d7fac24b063488c7ab8a16762b404815d41
Vincent Lombard <vincent.lombard@gmail.com>
2022-07-28 13:36:43 +0200

    test helm-doc

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 HELM_DOCS_VERSION="1.11.0"
-ARCHICTURE="Linux_x86_64"
+ARCHITECTURE="Linux_x86_64"
 
 # install helm-docs
 curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_${ARCHITECTURE}.tar.gz
